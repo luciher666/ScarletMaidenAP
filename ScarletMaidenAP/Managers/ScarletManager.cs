6ebbe8f5aba11043e121eae8a263dd5a092d806e
luciher666 <luciher666@proton.me>
2024-10-07 17:24:13 -0500

    Fix issue where sword would always be the first equipped weapon

--- ScarletMaidenAP/Managers/ScarletManager.cs
+++ ScarletMaidenAP/Managers/ScarletManager.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Random = UnityEngine.Random;
+﻿using Random = UnityEngine.Random;
 
 namespace ScarletMaidenAP.Managers
 {
@@ -12,6 +11,15 @@ namespace ScarletMaidenAP.Managers
             On.Scarlet.Awake += Scarlet_Awake;
             On.Scarlet.Start += Scarlet_Start;
             On.Scarlet.ResetItems += Scarlet_ResetItems;
+            On.Scarlet.LoadState += Scarlet_LoadState;
+        }
+
+        private void Scarlet_LoadState(On.Scarlet.orig_LoadState orig, Scarlet self)
+        {
+            var scarletState = GameManager.instance.GetSaveSlot().GetGameState().scarletState;
+            orig(self);
+            if (scarletState.activeWeaponStatsInstance == null)
+                self.SetActiveWeaponStatsInstance(GetWeapon(self).weaponStatsInstance);
         }
 
         private void Scarlet_Awake(On.Scarlet.orig_Awake orig, Scarlet self)
@@ -21,7 +29,6 @@ namespace ScarletMaidenAP.Managers
             {
                 Plugin.BepinLogger.LogWarning(spell.spell.title.GetLocalizedString());
             }
-            //if(Random.Range())
             self.defaultSpell = GetSpell(self);
             orig(self);
         }
@@ -35,7 +42,7 @@ namespace ScarletMaidenAP.Managers
             FistOfFire = 4,
             FireShield = 5,
             ArcaneSubmission = 6,
-            None = 7,
+            None = -1,
             Random = 99
         }
 
@@ -51,7 +58,7 @@ namespace ScarletMaidenAP.Managers
                     return self.defaultSpell;
                 case SpellType.Random:
                     return Random.Range(0, 7) == 0
-                        ? self.defaultSpell
+                        ? self.defaultSpell // arcane orb doesn't appear in level 1 loot table
                         : LootManager.instance.GetLevelSettings(1).GetRandomSpell();
                 default:
                     return LootManager.instance.GetLevelSettings(1).spellPool[(int) DefaultSpell - 1].spell;
@@ -67,39 +74,34 @@ namespace ScarletMaidenAP.Managers
             Random = 99
         }
 
-        public WeaponType DefaultWeapon = WeaponType.Random;
-        private void Scarlet_ResetItems(On.Scarlet.orig_ResetItems orig, Scarlet self)
+        public UIWeapon GetWeapon(Scarlet self)
         {
-            UIWeapon weapon;
             WeaponType weaponType = DefaultWeapon;
             if (weaponType == WeaponType.Random)
             {
-                weaponType = (WeaponType) Random.Range(0, 4);
+                weaponType = (WeaponType)Random.Range(0, 4);
             }
             switch (weaponType)
             {
                 case WeaponType.Spear:
-                    weapon = self.spear;
-                    break;
+                    return self.spear;
                 case WeaponType.Dagger:
-                    weapon = self.dagger;
-                    break;
+                    return self.dagger;
                 case WeaponType.Hammer:
-                    weapon = self.hammer;
-                    break;
-                case WeaponType.Random:
-                    Plugin.BepinLogger.LogError("WeaponType.Random was found during switch statement");
-                    throw new Exception("Please report this error on discord");
+                    return self.hammer;
                 case WeaponType.Sword:
                 default:
-                    weapon = self.sword;
-                    break;
+                    return self.sword;
             }
+        }
 
+        public WeaponType DefaultWeapon = WeaponType.Random;
+        private void Scarlet_ResetItems(On.Scarlet.orig_ResetItems orig, Scarlet self)
+        {
             self.activeAmuletInstance = null;
             self.activeRingInstance = null;
             self.activeSpellInstance = null;
-            self.SetActiveWeaponStatsInstance(weapon.weaponStatsInstance);
+            self.SetActiveWeaponStatsInstance(GetWeapon(self).weaponStatsInstance);
         }
 
         private void Scarlet_Start(On.Scarlet.orig_Start orig, Scarlet self)
