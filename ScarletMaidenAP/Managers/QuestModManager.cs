bd083e44c298c4ff2eaa7d6c7ef636b0220bb3c9
luciher666 <luciher666@proton.me>
2025-06-18 10:51:04 -0500

    Questsanity: Misty's dialog tree no longer ties to save state, always shows all quests and has a fail state in the LewdDungeonMenu to allow selecting quests where not enough items are sent

--- ScarletMaidenAP/Managers/QuestModManager.cs
+++ ScarletMaidenAP/Managers/QuestModManager.cs
@@ -1,6 +1,11 @@
-﻿using System.Linq;
-using ScarletMaidenAP.Utils;
+﻿using ScarletMaidenAP.Utils;
+using System;
+using System.Linq;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using Object = UnityEngine.Object;
+using Random = UnityEngine.Random;
 
 namespace ScarletMaidenAP.Managers
 {
@@ -12,6 +17,66 @@ namespace ScarletMaidenAP.Managers
             On.QuestManager.GetActiveQuestState += QuestManager_GetActiveQuestState;
             On.QuestManager.GetIsQuestItemAvailable += QuestManager_GetIsQuestItemAvailable;
             On.PauseMenuTabJournal.SetUpQuests += PauseMenuTabJournal_SetUpQuests;
+            On.Misty.OnInteract += Misty_OnInteract;
+            On.Misty.OnSelection1Selected += Misty_OnSelection1Selected;
+            On.LewdDungeonMenu.UpdateOptions += LewdDungeonMenu_UpdateOptions;
+            On.LewdDungeonMenu.OnOptionConfirmed += LewdDungeonMenu_OnOptionConfirmed;
+        }
+
+        private void Misty_OnSelection1Selected(On.Misty.orig_OnSelection1Selected orig, Misty self)
+        {
+            self.OpenLewdDungeonMenu();
+        }
+
+        /// <summary>
+        /// Display all quests in Misty's submenu.
+        /// </summary>
+        private void LewdDungeonMenu_UpdateOptions(On.LewdDungeonMenu.orig_UpdateOptions orig, LewdDungeonMenu self)
+        {
+            foreach (Component component in self.optionsLayoutGroup.transform)
+                Object.Destroy(component.gameObject);
+            self.quests.Clear();
+            self.options.Clear();
+            foreach (var quest in QuestManager.instance.quests.Where(q =>
+                         !q.title.GetLocalizedString().Equals("Archipelago")))
+            {
+                self.quests.Add(quest);
+                var gameObject = Object.Instantiate(self.listItemPrefab, Vector3.zero, Quaternion.identity, self.optionsLayoutGroup.transform);
+                gameObject.GetComponentInChildren<TextMeshProUGUI>().text = quest.title.GetLocalizedString();
+                self.options.Add(gameObject.GetComponent<Image>());
+            }
+            self.options.Add(self.leaveOption.GetComponent<Image>());
+            self.SetOptionSelected(0);
+        }
+
+        private void LewdDungeonMenu_OnOptionConfirmed(On.LewdDungeonMenu.orig_OnOptionConfirmed orig, LewdDungeonMenu self)
+        {
+            if (self.pointerIndex == self.options.Count - 1)
+            {
+                self.misty.ExitLewdDungeonMenu();
+            }
+            else
+            {
+                AudioManager.instance.PlaySFX("UI/ui_confirm");
+                // TODO: Conditional on if you've obtained enough quest items
+                self.gameObject.SetActive(false);
+                self.misty.activeDialog = self.misty.GetDialogWithID("quest_in_progress");
+                self.misty.hud.npcDialog.ShowDialog(self.misty, self.misty.activeDialog, self.misty.OnDialogCallback);
+                //self.misty.EnterLewdDungeon(self.quests[self.pointerIndex].lewdDungeonSceneName);
+            }
+        }
+
+        private void Misty_OnInteract(On.Misty.orig_OnInteract orig, Misty self)
+        {
+            if (self.isScarletInDialogPosition)
+            {
+                if (self.moveScarletToDialogPositionCoroutine != null || self.hud.npcDialog.gameObject.activeSelf || self.lewdDungeonMenu.gameObject.activeSelf)
+                    return;
+                self.activeDialog = self.GetDialogWithID("quest_in_progress_dungeon_menu_available");
+                self.hud.npcDialog.ShowDialog(self, self.activeDialog, self.OnDialogCallback);
+            }
+            else
+                self.MoveScarletToDialogPosition((Action)self.OnInteract);
         }
 
         private QuestState QuestManager_GetActiveQuestState(On.QuestManager.orig_GetActiveQuestState orig, QuestManager self)
@@ -38,7 +103,7 @@ namespace ScarletMaidenAP.Managers
             });
             self.activeQuestItem.text.text = "Archipelago";
             self.listItems.Add(self.activeQuestItem);
-            // TODO: Add all vanilla quests to the archive
+            // TODO: Add all vanilla quests to the archive, tracking progress from multiworld
 
             //foreach (QuestState completedQuestState in GameManager.instance.GetSaveSlot().gameState.journalState)
             //{
